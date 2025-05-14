e760131943d5cb4ce51e62c7c677c0f656b8504f
Timo Adam <ai.elite.circle@gmail.com>
2025-05-14 17:37:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1746865172684.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Update the following images in this component:
    
    Image 1:
             - Original image: /_next/image?url=https%3A%2F%2Fplacehold.co%2F80x80.png&w=256&q=75
             - New image URL: https://images.unsplash.com/photo-1500048993953-d23a436266cf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMnx8cGVyc29ufGVufDB8fHx8MTc0NzE2NjMyM3ww&ixlib=rb-4.1.0&q=80&w=1080
             - Hint used: "person avatar"
    
    Replace the image sources with the new URLs. This may involve updating src, srcset, background-image, or other relevant attributes depending on the element type.

--- src/components/landing/TestimonialsSection.tsx
+++ src/components/landing/TestimonialsSection.tsx
@@ -19,7 +19,7 @@ const testimonials = [
     name: 'Mike P.',
     role: 'Startup Founder',
     quote: "The market analysis feature is a game-changer. It gave us insights we hadn't even considered. Highly recommend!",
-    avatarUrl: 'https://placehold.co/80x80.png',
+    avatarUrl: 'https://images.unsplash.com/photo-1500048993953-d23a436266cf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMnx8cGVyc29ufGVufDB8fHx8MTc0NzE2NjMyM3ww&ixlib=rb-4.1.0&q=80&w=1080',
     stars: 5,
     hint: "person avatar",
   },
